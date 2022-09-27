54f982e0f4f1db281fba4da8872d493509b548ac
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-09-27 07:26:28 +0300

    last commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
+
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
-        onChange={props.handleFirstNameChange}
-        value={props.firstName}
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
       />
       <input
         type="text"
-        onChange={props.handleLastNameChange}
-        value={props.lastName}
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
       />
       <button type="submit">Submit</button>
     </form>
   );
 }
-
 export default Form;
